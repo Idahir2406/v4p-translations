53ac300609f05178773bb257e370dde7371b2810
Idahir2406 <ipincay5178@utm.edu.ec>
2026-02-27 22:58:08 -0500

    patch added to cors

--- v4p_translations_backend/src/main.ts
+++ v4p_translations_backend/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.enableCors({
     origin: allowedOrigins,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
